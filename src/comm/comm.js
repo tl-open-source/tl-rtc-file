88d40d39d31ebf69b3d4d3048901566ed642de94
tsm <tsm000000>
2022-07-23 19:03:33 +0800

    fix:ip兼容性问题

--- src/comm/comm.js
+++ src/comm/comm.js
@@ -5,10 +5,17 @@ const webrtcConf = conf.webrtc;
 
 //获取ip地址,初始化等相关配置
 function initData(req,res) {
-     let localIp = utils.getClientIP(req);
+     var regexIP =  /^((?:(?:25[0-5]|2[0-4]\d|[01]?\d?\d)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d?\d))$/;
+     let ip = utils.getLocalIP();
+     if (!regexIP.test(ip)) {
+          ip = utils.getClientIP(req)
+     }
+     if (!regexIP.test(ip)) {
+          ip = "127.0.0.1"
+     }
 
      let data = {
-          wsHost : wsConf.ws_online ? wsConf.ws_online : "ws://"+localIp+":"+wsConf.port,
+          wsHost : wsConf.ws_online ? wsConf.ws_online : "ws://"+ip+":"+wsConf.port,
           rtcConfig : {iceServers : webrtcConf.iceServers} ,
           options : webrtcConf.options
      };
