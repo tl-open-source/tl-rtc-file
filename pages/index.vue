09f3f0a243bad988b9bf74d2cf059ed971d52d38
yuguo138 <yuguo138@163.com>
2023-02-24 18:04:24 +0800

    新增首页头

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,11 @@
 <template>
-    <div>
-      <a-button @click="onClick">Home</a-button>
-      {{ test }}
-    </div>
+  <a-page-header class="solid-bottom" :ghost="true" title="WebRtc示例" sub-title="使用Webrtc视频聊天，传输文件" @back="() => null" />
 </template>
 <script setup>
 const { data: test } = await useFetch('/api/test');
-const onClick=async function(){
-    console.log(1);
-    
+const onClick = async function () {
+  console.log(1);
+
 
 }
 </script>
