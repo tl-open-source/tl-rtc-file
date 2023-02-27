c7a42536c113eb37885eca76938ba43a9cd60d86
yuguo138 <yuguo138@163.com>
2023-02-27 10:57:34 +0800

    首页改版

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,8 @@
 export default defineNuxtConfig({
   css: [
       'ant-design-vue/dist/antd.css',
-      '@/assets/css/main.css'
+      '@/assets/css/main.css',
+      '@/assets/css/icon.css'
   ],
   modules: [
       //'@nuxt/ui'
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,43 @@
 <template>
-  <a-page-header class="solid-bottom" :ghost="true" title="WebRtc示例" sub-title="使用Webrtc视频聊天，传输文件" @back="() => null" />
+  <div class="cu-bar bg-gradual-purple solid-bottom flex">
+    <div class="basis-xs flex">
+      <div class="padding-lr flex justify-center align-center text-xxl">
+        <span class="cuIcon-settings"></span>
+      </div>
+    </div>
+    <div class="basis-lg flex align-center justify-center text-xxl">WebRtc示例</div>
+    <div class="basis-xs flex justify-end">
+      <div class="padding-lr flex justify-center align-center text-xxl">
+        <span class="cuIcon-notice"></span>
+      </div>
+    </div>
+  </div>
+  <div class="flex align-center justify-center" style="height:200px;">
+    <div class="flex justify-center">
+      <a-space direction="vertical">
+        <a-input-number style="width:100%;" v-model:value="roomNo" placeholder="输入房间号" size="large" />
+        <a-space>
+          <a-button type="primary" size="large">
+            <template #icon>
+              <CheckOutlined />
+            </template>
+            创建/加入房间
+          </a-button>
+          <a-button size="large">
+            <template #icon>
+              <MessageOutlined />
+            </template>
+            与AI聊天
+          </a-button>
+        </a-space>
+      </a-space>
+    </div>
+  </div>
+  <a-result title="声明" sub-title="开源软件，仅可用户学习用途，其他用途后果自负。">
+  </a-result>
 </template>
 <script setup>
-const { data: test } = await useFetch('/api/test');
-const onClick = async function () {
-  console.log(1);
-
-
-}
+import { ref } from 'vue';
+import { CheckOutlined, MessageOutlined } from "@ant-design/icons-vue";
+const roomNo = ref(10086);
 </script>
